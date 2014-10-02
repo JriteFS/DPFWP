538f41d4752390c81b5d59015d31d9d396e52ecb
JriteFS <jrite86@yahoo.com>
2014-10-01 21:54:08 -0500

    added

--- Lab 1 Madlib/madlib.py
+++ Lab 1 Madlib/madlib.py
@@ -1,16 +1,18 @@
 __author__ = 'jacobritenour'
 
-
+#initial questions
 response = raw_input("Please enter your name: ")
 response1 = raw_input("Please enter your age: ")
 response2 = raw_input("Please enter a number between 1-15: ")
-response3 = raw_input("Please enter your favorite color: ")
-response4 = raw_input("Please enter your favorite outdoor activity: ")
-response5 = raw_input("Please enter your favorite indoor activity: ")
-response6 = raw_input("Are you married? Yes or No: ")
-response7 = raw_input("Do you have children? Yes or No: ")
+response3 = raw_input("Please enter a number between 5-30: ")
+response4 = raw_input("Please enter your favorite color: ")
+response5 = raw_input("Please enter your favorite outdoor activity: ")
+response6 = raw_input("Please enter your favorite indoor activity: ")
+response7 = raw_input("Are you married? Yes or No: ")
+response8 = raw_input("Do you have children? Yes or No: ")
 
-
+#testing responses
+'''
 print response
 print "Hello there, ", response
 print response1
@@ -20,7 +22,7 @@ print response4
 print response5
 print response6
 print response7
-
+'''
 
 
 
