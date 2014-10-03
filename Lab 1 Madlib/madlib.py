8f424f8507cb47bde17b03cbb579100f619f57c5
JriteFS <jrite86@yahoo.com>
2014-10-02 19:19:41 -0500

    making changes

--- Lab 1 Madlib/madlib.py
+++ Lab 1 Madlib/madlib.py
@@ -8,11 +8,12 @@ response3 = raw_input("Please enter a number between 1-5: ")
 response4 = raw_input("Please enter your favorite color: ")
 response5 = raw_input("Please enter your favorite outdoor activity: ")
 response6 = raw_input("Please enter your favorite indoor activity: ")
-response7 = raw_input("Are you married? Yes or No: ")
-response8 = raw_input("Do you have children? Yes or No: ")
+response7 = raw_input("Are you married? 1 for Yes or 2 for No: ")
+response8 = raw_input("Do you have children? 1 for Yes or 2 for No: ")
 response9 = raw_input("If you could be an animal what would it be: ")
 response10 = raw_input("If you could have a Super Power what would it be: ")
 
+names = ["Bobby","James","Chelsea","Jonna","Riley"]
 
 print "It is nice to finally meet you " + response + " I am Dr. Jogue"
 
@@ -28,7 +29,15 @@ print "The type of serum would have caused your to become a " + response4 + " co
 print "We observed you doing the " + response5 + " activity one day last week at the beach."
 
 
-if response7 = "yes":
-    print "We here at the lab are happy to see also that during your " + response6 + " activities "
+if response7 < 1:
+    print "We here at the lab are happy to see also that during your " + response6 + " activities you were able to find a mate."
 else:
     print "We see that with all of your " + response6 + " activities have yet to find a suitable and sustainable mate. Humm we must correct that."
+
+print "Now we just need to add " + response3 + " offspring in the mix. "
+
+for response3 in range(1,5):
+    print "We are ready for offspring ", response2
+
+print "Ohhhh wont it be fun to have mini " + response9 + " that have special abilities like " + response10
+
