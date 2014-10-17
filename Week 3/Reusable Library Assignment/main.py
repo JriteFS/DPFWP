864c637c5a01a1cee977b30866ff050c803ed5f7
JriteFS <jrite86@yahoo.com>
2014-10-16 22:41:37 -0500

    9th commit

--- Week 3/Reusable Library Assignment/main.py
+++ Week 3/Reusable Library Assignment/main.py
@@ -1,36 +1,57 @@
 __author__ = 'jacobritenour'
 
-
+'''
+Jacob Ritenour
+10-16-2014
+DPFWP
+Reusable Library Assignment
+'''
 import webapp2
 from lib import CarData, FavoriteCars
-from page import FormPage, ResultsPage
+from page import ResultsPage
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
 
         #page for form
         f = FormPage()
-        f.
+
+
+class FormPage(object):
+    def get(self): #function that is going to make everything work
+
+        fmlib = FavoriteCars()
+        form_top = '''
+<!DOCTYPE HTML>
+<html style="background: tan; max-width: 960px; margin:0 auto;">
+    <head>
+        <title>Favorite vehicle</title>
+    </head>
+    <body>
+    <h1 style="text-align:center;">Enter Your Favorite Vehicle</h1>
+    <h3 style="text-align:right;">Create a DataBase of your favorite vehicles here!!</h3>
+    <h3 style="text-align:center;">Fill out the form below and begin today!!!</h3>
+    '''
+        form_mid = '''<form method="GET">
+            <label>make</label><input type="text" name="make" />
+            <label>Model</label><input type="text" name="model" />
+            <label>Year</label><input type="number" name="year" />
+            <input type="submit" value="Submit" />'''
+        form_end = '''
+        </form>
+    </body>
+</html>'''
+        if self.request.GET:
+            #stores info that came from the form to be used later
+            make = self.request.GET['make']#add this to the array
+            model = self.request.GET['model']
+            year = self.request.GET['year']
+            fmlib.add_car(make,model,year)
+
+
 
         #page for class
         p = ResultsPage()
-        fmlib = FavoriteCars()
-
-        md1 = CarData()#calling the class
-        md1.title = "The Princess Bride"
-        md1.year = 1989 #going to call a function
-        md1.director = "Rob Reiner"
-        fmlib.add_car(md1)
-
-        md2 = CarData()#calling the class
-        md2.title = "Dune"
-        md2.year = 1986 #going to call a function
-        md2.director = "David Lynch"
-        fmlib.add_car(md2)
-
-       #movie title
-        #year movie was made
-        #director of the film
 
         p.body = fmlib.compile_list() + fmlib.calc_time_span()
         self.response.write(p.print_out())
--- Week 3/Reusable Library Assignment/page.py
+++ Week 3/Reusable Library Assignment/page.py
@@ -1,42 +1,5 @@
 __author__ = 'jacobritenour'
 
-class FormPage(object):
-    def get(self): #function that is going to make everything work
-        page_head = '''
-<!DOCTYPE HTML>
-<html style="background: tan; max-width: 960px; margin:0 auto;">
-    <head>
-        <title>Favorite vehicle</title>
-    </head>
-    <body>
-    <h1 style="text-align:center;">Enter Your Favorite Vehicle</h1>
-    <h3 style="text-align:right;">Create a DataBase of your favorite vehicles here!!</h3>
-    <h3 style="text-align:center;">Fill out the form below and begin today!!!</h3>
-    '''
-        page_body = '''<form method="GET">
-            <label>make</label><input type="text" name="make" />
-            <label>Model</label><input type="text" name="model" />
-            <label>Year</label><input type="number" name="year" />
-            <input type="submit" value="Submit" />'''
-        page_end = '''
-        </form>
-        <p style="background:brown; text:white; text-align:center;">We hope you enjoy your Fish!!</p>
-        <footer style="background:brown; text:white;">Be Aware that one of our personnel when be sending you an email to verify the available living space and your ability to care for this animal. They may also make a home visit just to ensure the safety of all wildlife handled by our organization. If you refuse to allow our personnel we can/will deny you from being the proud parent of an amazing creature of the sea.</footer>
-    </body>
-</html>'''
-        if self.request.GET:
-            #stores info that came from the form to be used later
-            make = self.request.GET['make']#add this to the array
-            model = self.request.GET['model']
-            year = self.request.GET['year']
-
-            self.response.write(page_head #listing everything i want to show
-                                + make + ''
-                                + model + ''
-                                + year + ''
-                                + page_end) #this will display the info placed in but not the body
-        else:
-            self.response.write(page_head + page_body + page_end) #Printing the info this will display every as normal
 
 
 
