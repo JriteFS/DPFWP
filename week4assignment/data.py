7b5ad7b42651d492b996d9329c7205e07fc1223b
JriteFS <jrite86@yahoo.com>
2014-10-22 21:56:53 -0500

    added a class to the data page

--- week4assignment/data.py
+++ week4assignment/data.py
@@ -1,3 +1,6 @@
 __author__ = 'jacobritenour'
 
 
+class DataObject(object):#created the class for data object
+    def __init__(self):# initiate the class
+        pass
--- week4assignment/main.py
+++ week4assignment/main.py
@@ -7,6 +7,7 @@ Week 4 Assignment Dynamic Site
 import webapp2
 from pages import Page
 
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
 
