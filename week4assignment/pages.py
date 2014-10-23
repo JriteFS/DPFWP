706733c4dcb41156e4c1c24ce4e41a1d7554c0f0
JriteFS <jrite86@yahoo.com>
2014-10-22 22:08:03 -0500

    added section with topic to the pages.py page

--- week4assignment/pages.py
+++ week4assignment/pages.py
@@ -7,9 +7,14 @@ class Page(object):#adding Page class to load initial page
 <!DOCTYPE HTML>
 <html>
     <head>
-        <title></title>
+        <title>Types of SuperNatural Beings</title>
     </head>
-    <body>'''
+    <body>
+        <section>
+            <p>There are many types of different supernatural beings that have been thought up through out history. Listed below are a few that you can review and see which one you most closely resemble. The abilities of these creatures or beings can range from immortality or shape-shifting to super-speed and the strength of 100 men. There has been such a large imagination on creating different characters that in order to be unique you have to combine several of these abilities. Vampires, Werewolves, Mermaids/Mermen, Witches/Warlocks, and a few others are listed below so you can compare and possibly relate. </p>
+        </section>
+
+    '''
 
         self._body = ''
         self._close = '''
