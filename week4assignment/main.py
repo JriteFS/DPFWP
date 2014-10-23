c9bceb0e6659b26c344a33cb319ec6d3a889c72a
JriteFS <jrite86@yahoo.com>
2014-10-22 21:58:36 -0500

    added import and instance of DataObject to main

--- week4assignment/main.py
+++ week4assignment/main.py
@@ -6,12 +6,14 @@ Week 4 Assignment Dynamic Site
 '''
 import webapp2
 from pages import Page
+from data import DataObject
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
 
         p = Page()# created instance of Page
+        d = DataObject() #created instance of Data Object
         self.response.write(p.print_initial())
 
 
