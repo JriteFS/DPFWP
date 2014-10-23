ce2d2a0b1e4908cb88c902a9c27cfc217bac47c8
JriteFS <jrite86@yahoo.com>
2014-10-22 20:14:12 -0500

    created import on main.py for Page class

--- week4assignment/main.py
+++ week4assignment/main.py
@@ -5,10 +5,15 @@ DPFWP
 Week 4 Assignment Dynamic Site
 '''
 import webapp2
+from pages import Page
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
+
+        p = Page
+
+        print
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
